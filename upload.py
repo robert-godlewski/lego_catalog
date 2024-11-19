70240e1ab484de138e492bf2b121b1b09c2e9eaa
Robert Godlewski <robtechg5@gmail.com>
2024-11-18 18:44:03 -0800

    started going through the data

--- upload.py
+++ upload.py
@@ -1,5 +1,5 @@
 import sqlite3
-import numpy # type: ignore
+import numpy as np # type: ignore
 import pandas as pd # type: ignore
 
 
@@ -22,26 +22,54 @@ except sqlite3.OperationalError:
         print("Create new table failed")
 
 filename = input("What is the name of the csv file (without adding in csv at the end)? ")
+# Mainly for testing
+if len(filename) < 1:
+    filename = "MyLegoSets - Tolkien"
 filename += ".csv"
 
-group_prompt = "What is the group called that we are specifically uploading to (leave blank if there is not a specific group)? "
-group_name = input(group_prompt)
-cur.execute("INSERT OR IGNORE INTO lego_group (name) VALUES ( ? )", (group_name, ))
-cur.execute("SELECT * FROM lego_group WHERE name = ? ", (group_name, ))
-group_data = cur.fetchone()
-group = {
-    "id": group_data[0],
-    "name": group_data[1]
-}
-print(group)
-
 try:
-    data = pd.read_csv(filename, header=0).values
-    print(type(data))
-    print(data)
-    print(f"Shape of data = {data.shape}")
-    print(f"Number of data points in the data = {data.shape[0]}")
+    data_raw = pd.read_csv(filename).values
+    print(f"Shape of data = {data_raw.shape}")
+    print(f"Number of data points in the data = {data_raw.shape[0]}")
+    # Header - Need to find something that actually takes the header info out
+    header = data_raw[0]
+    print(header)
+    group = {} # Maybe make a function to get this since it looks pretty repetitive
+    # Might need to get more sections to add in later for Theme and Series
+    theme = {}
+    series = {}
+    if len(header) < 11:
+        group_prompt = "What is the group called that we are specifically uploading to (leave blank if there is not a specific group)? "
+        group_name = input(group_prompt)
+        # ***Repeat start***
+        cur.execute("INSERT OR IGNORE INTO lego_group (name) VALUES ( ? )", (group_name, ))
+        cur.execute("SELECT * FROM lego_group WHERE name = ? ", (group_name, ))
+        group_data = cur.fetchone()
+        group = {
+            "id": group_data[0],
+            "name": group_data[1]
+        }
+        # ***Repeat end****
+    data = np.delete(data_raw, 0, 0)
+    # print(type(data))
+    # print(data)
     # Need to go through each piece of data and add it to the database
+    for row in range(len(data)-1):
+        print(f"row {row} = {data[row]}")
+        if "id" not in group:
+            group_name = data[row][0]
+            print(group_name)
+            # ***Repeat start***
+            cur.execute("INSERT OR IGNORE INTO lego_group (name) VALUES ( ? )", (group_name, ))
+            cur.execute("SELECT * FROM lego_group WHERE name = ? ", (group_name, ))
+            group_data = cur.fetchone()
+            group = {
+                "id": group_data[0],
+                "name": group_data[1]
+            }
+            # ***Repeat end****
+        print(f"Lego Group = {group}")
+        # Need the theme, series, kit, and parts here
 except:
     print("There is nothing to upload here.")
 

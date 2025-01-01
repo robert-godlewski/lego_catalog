4d5316242253324270acfc5fabf488c83e643271
Robert Godlewski <robtechg5@gmail.com>
2024-12-31 19:23:24 -0800

    fixed create table scripts and added in a VS debugger

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ For now we are just building up a database that has a collection of lego sets.
 * [SQLite Documentation](https://www.sqlite.org/lang.html)
 * [Python sqlite3 Documentation](https://docs.python.org/3/library/sqlite3.html)
 * [NumPy Array Manipulation Routines Documentation](https://numpy.org/doc/stable/reference/routines.array-manipulation.html)
+* [Building out a Debugger within VS Code](https://code.visualstudio.com/docs/python/debugging)
--- create_db.py
+++ create_db.py
@@ -3,89 +3,68 @@ import sqlite3
 
 con = sqlite3.connect("db.sqlite3")
 cur = con.cursor()
-# Creating our tables - need to add in all of the others - Might be able to make a function or make a separate script for this.
-# For lego_group
-try:
-    cur.execute("SELECT * FROM lego_group")
-    print("lego_group already exists.")
-except sqlite3.OperationalError:
-    msg = ""
+
+# Creating our tables
+def _createTable(table_name: str, script: str):
     try:
-        print("Creating a lego_group table...")
-        cur.execute("""CREATE TABLE IF NOT EXISTS lego_group (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT, 
-                    name TEXT UNIQUE
-        )""")
-        msg += "Successfully created "
-    except:
-        msg += "Failed to create "
-    msg += "new lego_group table."
-    print(msg)
+        cur.execute(f"SELECT * FROM {table_name}")
+        print(f"{table_name} already exists.")
+    except sqlite3.OperationalError:
+        msg = ""
+        try:
+            print(f"Creating {table_name} table...")
+            cur.execute(script)
+            msg += "Successfully created "
+        except:
+            msg += "Failed to create "
+        msg += f"new {table_name} table."
+        print(msg)
+
+# For lego_group
+group_script = """
+CREATE TABLE IF NOT EXISTS lego_group (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT UNIQUE
+);
+"""
+_createTable("lego_group",group_script)
 
 # For lego_theme
-try:
-    cur.execute("SELECT * FROM lego_theme")
-    print("lego_theme already exists.")
-except sqlite3.OperationalError:
-    msg = ""
-    try:
-        print("Creating a lego_theme table...")
-        cur.execute("""CREATE TABLE IF NOT EXISTS lego_theme (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    name TEXT UNIQUE,
-                    group_id REFERENCES lego_group (id) ON DELETE CASCADE
-        )""")
-        msg += "Successfully created "
-    except:
-        msg += "Failed to create "
-    msg += "new lego_theme table."
-    print(msg)
+theme_script = """
+CREATE TABLE IF NOT EXISTS lego_theme (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT UNIQUE,
+    group_id REFERENCES lego_group (id) ON DELETE CASCADE
+);
+"""
+_createTable("lego_theme",theme_script)
 
 # For lego_series
-try:
-    cur.execute("SELECT * FROM lego_series")
-    print("lego_series already exists.")
-except sqlite3.OperationalError:
-    msg = ""
-    try:
-        print("Creating a lego_series table...")
-        cur.execute("""CREATE TABLE IF NOT EXISTS lego_series (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    name TEXT UNIQUE,
-                    theme_id REFERENCES lego_theme (id) ON DELETE CASCADE
-        )""")
-        msg += "Successfully created "
-    except:
-        msg += "Failed to create "
-    msg += "new lego_series table."
-    print(msg)
+series_script = """
+CREATE TABLE IF NOT EXISTS lego_series (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT UNIQUE,
+    theme_id REFERENCES lego_theme (id) ON DELETE CASCADE
+);
+"""
+_createTable("lego_series",series_script)
 
-# For lego_kit
-try:
-    cur.execute("SELECT * FROM lego_kit")
-    print("lego_kit already exists.")
-except sqlite3.OperationalError:
-    msg = ""
-    try:
-        print("Creating a lego_kit table...")
-        # Might need to fix this actually
-        # * complete_kit should be a boolean value default as false
-        # * for_sale should be a boolean value default as false
-        cur.execute("""CREATE TABLE IF NOT EXISTS lego_kit (
-                    id INTEGER PRIMARY KEY,
-                    name TEXT UNIQUE,
-                    number_sets INTEGER DEFAULT 1,
-                    complete_kit INTEGER DEFAULT 0,
-                    for_sale INTEGER DEFAULT 0,
-                    box_location TEXT,
-                    notes TEXT
-                    series_id REFERENCES lego_series (id) ON DELETE CASCADE
-        )""")
-        msg += "Successfully created "
-    except:
-        msg += "Failed to create "
-    msg += "new lego_kit table."
-    print(msg)
+# For lego_kit - Might need to fix this
+# * complete_kit should be a boolean value default as false
+# * for_sale should be a boolean value default as false
+kit_script = """
+CREATE TABLE IF NOT EXISTS lego_kit (
+    id INTEGER PRIMARY KEY,
+    name TEXT UNIQUE,
+    number_sets INTEGER DEFAULT 1,
+    complete_kit INTEGER DEFAULT 0,
+    for_sale INTEGER DEFAULT 0,
+    box_location TEXT,
+    notes TEXT
+    series_id REFERENCES lego_series (id) ON DELETE CASCADE
+);
+"""
+_createTable("lego_kit",kit_script)
 
 # For lego_parts
 # * id (part number) - unique
